eb1a27d8bcfede068c4ae1c3617e3fffd4ebfca6
Craig Munro <cah@mac.com>
2026-05-24 20:48:01 -0400

    cidrwalk 0.1.1

--- Formula/cidrwalk.rb
+++ Formula/cidrwalk.rb
@@ -1,8 +1,8 @@
 class Cidrwalk < Formula
   desc "Summarize explicit IPv4 or IPv6 ranges into CIDR prefixes"
   homepage "https://github.com/RouteObjects/cidrwalk"
-  url "https://github.com/RouteObjects/cidrwalk/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "dd1a10b336164f748fa702aa00806f9cb5f78bcc9b7da1d202521b14379aa5d3"
+  url "https://github.com/RouteObjects/cidrwalk/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "886298cb2abbdf7ae65d15593ef7c3882fb551fe2b62d5df0ded752c00e4c22b"
   license "Apache-2.0"
 
   depends_on xcode: ["26.5", :build]
@@ -14,6 +14,7 @@ class Cidrwalk < Formula
   end
 
   test do
+    assert_equal "0.1.1\n", shell_output("#{bin}/cidrwalk --version")
     assert_equal "192.0.2.0/24\n",
       shell_output("#{bin}/cidrwalk addresses 192.0.2.0/32 192.0.2.255/32")
   end
